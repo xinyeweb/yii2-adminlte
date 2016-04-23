e154aedf5dfe1d8214f18ec5bb7eb122b06f53bd
hotar.zhang@gmail.com <hotar.zhang@gmail.com>
2016-04-23 10:33:39 +0800

    edit content.php

--- demo/content.php
+++ demo/content.php
@@ -2,236 +2,27 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            UI
-            <small>UI优化</small>
+            <?php
+            use yii\widgets\Breadcrumbs;
+
+            if ($this->title !== null) {
+                echo $this->title;
+            } else {
+                echo \yii\helpers\Inflector::camel2words(\yii\helpers\Inflector::id2camel($this->context->module->id));
+                echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Module</small>' : '';
+            } ?>
         </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> XY ADMIN</a></li>
-            <li class="active">UI</li>
-        </ol>
+        <?=Breadcrumbs::widget(
+            [
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]
+        ) ?>
+
     </section>
 
     <!-- Main content -->
     <section class="content">
-
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <form>
-            <div class="form-group">
-                <input type="file" id="file" class="inputfile">
-                <label for="file"><i class="glyphicon glyphicon-cloud-upload"></i>选择上传文件</label>
-            </div>
-        </form>
-
-        <div class="panel panel-default">
-            <div class="panel-body">
-                弹出框
-            </div>
-        </div>
-        <form>
-            <div class="form-group">
-                <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".shop-wifi-status">离线</button>
-
-                <div class="modal fade shop-wifi-status">
-                    <div class="vertical-alignment-helper">
-                        <div class="modal-dialog vertical-align-center">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                                    <h4 class="modal-title">測試路由器</h4>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="box-body no-padding">
-                                        <table class="table">
-                                            <tbody>
-                                            <tr><td>路由器標記</td><td class="route-title">測試路由器</td></tr>
-                                            <tr><td>離線時間</td><td class="route-lasttime">2016-03-24 09:18:15</td></tr>
-                                            <tr><td>所屬店鋪</td><td class="route-shop">測試店鋪</td></tr>
-                                            <tr><td>聯絡電話</td><td class="route-phone">17707561655</td></tr>
-                                            </tbody>
-                                        </table>
-                                    </div><!-- /.box-body -->
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!--/vertical-alignment-helper-->
-                </div>
-                <!--/modal-->
-            </div>
-        </form>
-        <!-- Your Page Content Here -->
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-body">
-                文件上传框
-            </div>
-        </div>
-
-
-
-
-
+        <?=$content?>
     </section>
     <!-- /.content -->
 </div>
