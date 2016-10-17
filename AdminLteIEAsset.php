4bf5d9cc9789d409bcb354fca369b8aa2ad01a95
hoterzhang <232767044@qq.com>
2016-10-17 18:10:42 +0800

    add IE assests

---
+++ AdminLteIEAsset.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: hoter
+ * Date: 2016/4/22
+ * Time: 14:30
+ */
+
+namespace xinyeweb\adminlte;
+
+use yii\base\Exception;
+use yii\web\AssetBundle;
+
+class AdminLteIEAsset extends AssetBundle
+{
+
+    public $css = [];
+
+    public $js = [
+        'js/html5shiv.min.js',
+        'plugins/respond.min.js',
+    ];
+
+    public $jsOptions = [
+        'condition' => 'It IE 9',
+    ];
+
+    public function init() {
+        $this->sourcePath = dirname(__FILE__) .DIRECTORY_SEPARATOR . 'assets';
+        parent::init();
+    }
+}
